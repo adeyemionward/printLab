fc7c8d926619b5bf28e8d018d84dbc74eb4b3cbc
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2024-02-02 13:42:11 +0100

    modified the updated migration

--- database/migrations/2024_02_02_115904_add_updated_by_to_testimonials.php
+++ database/migrations/2024_02_02_115904_add_updated_by_to_testimonials.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('testimonials', function (Blueprint $table) {
-            $table->integer('updated_by');
+            $table->integer('updated_by')->nullable();
         });
     }
 
