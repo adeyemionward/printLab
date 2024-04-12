04fece2bb77777afc73b6c529661c2c237f04aca
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2024-04-12 05:49:45 +0100

    soft and hard cover on video brochure page

--- resources/views/video_brochure/index.blade.php
+++ resources/views/video_brochure/index.blade.php
@@ -272,8 +272,8 @@ $(document).ready(function() {
                 // Iterate through the response and append HTML for each product
             $.each(response.video_profiling, function(index, val) {
                 htmlContent += '<div class="col-lg-3 col-md-6 col-sm-12 mb-4">';
-                htmlContent += '<div class="card">';
-                htmlContent += '<img src="' + (val.image ? '{{ asset('storage/images/') }}/' + val.image : '') + '" alt="product_image" style="width: 100%; height: 320px;">';
+                htmlContent += '<div class="card border-0">';
+                htmlContent += '<img src="' + (val.image ? '{{ asset('storage/images/') }}/' + val.image : '{{ asset('public/storage/images/') }}/' + val.image) + '" alt="product_image" style="width: 100%; height: 320px;">';
                 htmlContent += '<div class="card-body">';
                 htmlContent += '<h5 class="card-title">' + val.title + '</h5>';
                 htmlContent += '<p class="card-text">&#8358; 2000</p>'; // Dummy price in Nigerian Naira
