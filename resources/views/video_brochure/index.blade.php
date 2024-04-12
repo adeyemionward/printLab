45a1f9af706c372e2817bac56ab6fd1515277412
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2024-04-12 05:44:59 +0100

    video responsiveness

--- resources/views/video_brochure/index.blade.php
+++ resources/views/video_brochure/index.blade.php
@@ -102,41 +102,35 @@ label {
 <div class=" slider-height d-flex align-items-center">
     <div class="container">
         <div class="row">
-
-
-            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6  col-sm-12">
-                @if ( env('APP_ENV') == 'local')
-                <video  height="350" width="500" controls>
-                    <source src="{{asset('video/video_profile_1.mp4')}}" type="video/mp4">
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                @if (env('APP_ENV') == 'local')
+                <video height="350" width="100%" controls>
+                    <source src="{{ asset('video/video_profile_1.mp4') }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
                 @else
-                <video  height="350" width="500" controls>
-                    <source src="{{asset('public/video/video_profile_1.mp4')}}" type="video/mp4">
+                <video height="350" width="100%" controls>
+                    <source src="{{ asset('public/video/video_profile_1.mp4') }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
                 @endif
-
-
-
             </div>
-
-            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6  col-sm-12">
-                @if ( env('APP_ENV') == 'local')
-                <video  height="350" width="500px" controls>
-                    <source src="{{asset('video/video_profile_2.mp4')}}" type="video/mp4">
+        
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                @if (env('APP_ENV') == 'local')
+                <video height="350" width="100%" controls>
+                    <source src="{{ asset('video/video_profile_2.mp4') }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
                 @else
-                <video  height="350" width="500px" controls>
-                    <source src="{{asset('public/video/video_profile_2.mp4')}}" type="video/mp4">
+                <video height="350" width="100%" controls>
+                    <source src="{{ asset('public/video/video_profile_2.mp4') }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
                 @endif
-
-
             </div>
         </div>
+        
     </div>
 </div>
 
