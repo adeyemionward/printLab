f678316648ed097573eec7da8e51d3f5e0cde201
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2024-05-07 17:11:32 +0100

    admin view profilr

--- resources/views/admin/users/view_profile.blade.php
+++ resources/views/admin/users/view_profile.blade.php
@@ -23,7 +23,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="row">
-                                @include('users.profile_inc')
+                                @include('admin.users.profile_inc')
                                 <div class="col-md-9 col-xl-9">
                                     <div class="card">
                                         <div class="card-header bg-white">
